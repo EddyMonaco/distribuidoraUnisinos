17c14b74ab9858d687014c51dd5f02da5608dc6c
Jackson Pandolfo <jacksonpandolfo@hotmail.com>
2025-10-06 23:11:27 -0300

    Ajustes nas models de supervisor e vendedor

--- src/main/java/com/distribuicao/unisinos/model/Supervisor.java
+++ src/main/java/com/distribuicao/unisinos/model/Supervisor.java
@@ -2,8 +2,10 @@ package com.distribuicao.unisinos.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "usuarios")
 @DiscriminatorValue("SUPERVISOR")
 public class Supervisor extends Usuario {
 
--- src/main/java/com/distribuicao/unisinos/model/Vendedor.java
+++ src/main/java/com/distribuicao/unisinos/model/Vendedor.java
@@ -2,8 +2,10 @@ package com.distribuicao.unisinos.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "usuarios")
 @DiscriminatorValue("VENDEDOR")
 public class Vendedor extends Usuario {
 }
