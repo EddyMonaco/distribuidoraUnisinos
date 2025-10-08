20bc0cffce178093e4b0afc6eab41ba3938c2675
Jackson Pandolfo <jacksonpandolfo@hotmail.com>
2025-10-08 19:38:37 -0300

    Adicionado configuração do Swagger

---
+++ src/main/java/com/distribuicao/unisinos/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.distribuicao.unisinos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.distribuicao.unisinos.service.MessageService;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Autowired
+	private MessageService messageService;
+	
+	@Value("${app.version}")
+	private String version;
+	
+	@Bean
+	public OpenAPI getOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title(messageService.getMessage("app.title"))
+				.version(version)
+				.description(messageService.getMessage("app.description")));
+	}
+
+}
---
+++ src/main/java/com/distribuicao/unisinos/service/MessageService.java
@@ -0,0 +1,22 @@
+package com.distribuicao.unisinos.service;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	public String getMessage(String key) {
+		return getMessage(key, null);
+	}
+	
+	public String getMessage(String key, Object[] objects) {
+		return messageSource.getMessage(key, objects, Locale.getDefault());
+	}
+}
